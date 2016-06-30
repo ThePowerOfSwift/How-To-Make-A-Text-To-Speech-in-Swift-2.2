c4fff68e03fdc50b89535e8229f74e28416a91ce
Frezy Stone Mboumba <FrezyStone@macbook-pro-de-frezy.local>
2016-06-30 12:57:02 -0500

    Final Project

--- TextToSpeech Demo/ViewController.swift
+++ TextToSpeech Demo/ViewController.swift
@@ -7,19 +7,66 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVSpeechSynthesizerDelegate {
 
+    @IBOutlet weak var textView: UITextView!
+    
+    var synthesizer = AVSpeechSynthesizer()
+    var totalUtterance: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        textView.text = "Cuando madruga para ir a la escuela, Basilio está contento y se viste feliz con el uniforme de su colegio. A Basilio no le importa caminar todos los kilómetros que separan su humilde casa de la escuela, ya que hace el camino pensando en su futuro y olvidando su presente. De esta forma, cruza los caminos silbando y saltando entre los pedregales, solo, pero acompañado de su esperanza y la de su madre que suele vigilar la primera parte del agreste recorrido."
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    @IBAction func play(sender: AnyObject) {
+        if !self.synthesizer.speaking{
+            
+            let textParagraphs = self.textView.text.componentsSeparatedByString("\n")
+            self.totalUtterance = textParagraphs.count
+            
+            for pieceOfText in textParagraphs {
+                
+                let speechUtterance = AVSpeechUtterance(string: pieceOfText)
+                let voice = AVSpeechSynthesisVoice(language: "es-MX")
+                speechUtterance.voice = voice
+                let voices = AVSpeechSynthesisVoice.speechVoices()
+                print(voices)
+                self.synthesizer.speakUtterance(speechUtterance)
+                
+            }
+            
+        }else {
+            self.synthesizer.continueSpeaking()
+        }
+        
+        
     }
-
+    
+    
+    
+    
+    @IBAction func pause(sender: AnyObject) {
+    self.synthesizer.pauseSpeakingAtBoundary(.Word)
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+   self.synthesizer.stopSpeakingAtBoundary(.Immediate)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
